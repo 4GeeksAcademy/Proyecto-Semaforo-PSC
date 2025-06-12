3e3a73d6e1f39297297d4689a4fc537aaacbb04c
Pascual Rivera M <priveram87@gmail.com>
2025-06-12 23:33:45 +0000

    revision responsive

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
-import PropType from "prop-types"
+import PropTypes from "prop-types";
 
+const Semaforo = ({ onColorSelect }) => {
+    const [selected, setSelected] = useState("");
 
-const Semaforo = () => {
+    const handleClick = (color) => {
+        setSelected(color);
+        onColorSelect(color);
+    };
 
-    const [selected, setSelected] = useState("")
     return (
         <div className="container">
-            <div className={`redLight ${selected === "redLight" ? "redOn" : ""}`} onClick={() => setSelected("redLight")}></div>
-            <div className={`yellowLight ${selected === "yellowLight" ? "yellowOn" : ""}`} onClick={() => setSelected("yellowLight")}></div>
-            <div className={`greenLight ${selected === "greenLight" ? "greenOn" : ""}`} onClick={() => setSelected("greenLight")}></div>
-        </div >
-    )
+            <div className={`redLight ${selected === "redLight" ? "redOn" : ""}`} onClick={() => handleClick("redLight")}></div>
+            <div className={`yellowLight ${selected === "yellowLight" ? "yellowOn" : ""}`} onClick={() => handleClick("yellowLight")}></div>
+            <div className={`greenLight ${selected === "greenLight" ? "greenOn" : ""}`} onClick={() => handleClick("greenLight")}></div>
+        </div>
+    );
 };
 
-
+Semaforo.propTypes = {
+    onColorSelect: PropTypes.func.isRequired,
+};
 
 export default Semaforo;
